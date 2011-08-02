b1f50cb1b482c0da7fbace3a6346d5f3fecf095e
Olivier Detour <o.detour@netheos.net>
2011-08-02 11:53:31 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Netheos::Application.config.secret_token = 'bd3b10c1fa5663ddf294c46a58e365a662db54adc3f2029224db6da4ad062f7cf55c912abd5a6f05ba59b8bbba25a6da186faa74b2e74547084cc6ebdeb132a2'
