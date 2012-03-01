35f62fd97d87f8273932d5fa4f39d5e4858c0f98
Silmaneous <silmaneous@gmail.com>
2012-03-01 19:55:47 +0100

    lightbox patch

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -7,26 +7,44 @@ class SolutionsController < ApplicationController
 
   def web_access_protection
     @title = @@title
+    
+    @header = false
+    @footer = false
   end
 
-  def document_privacy_protection
+  def document_privacy
     @title = @@title
+
+    @header = false
+    @footer = false
   end
 
   def instant_contract_signing
     @title = @@title
+
+    @header = false
+    @footer = false
   end
 
   def financial_institutions
     @title = @@title
+    
+    @header = false
+    @footer = false
   end
   
   def device_manufacturers
     @title = @@title
+
+    @header = false
+    @footer = false
   end
   
   def software_compagnies
     @title = @@title
+
+    @header = false
+    @footer = false
   end
   
 end
