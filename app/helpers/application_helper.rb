30e34cb426b75bcf7d34f9a367f6a46afd283750
Olivier Detour <o.detour@netheos.net>
2012-12-03 10:25:16 +0100

    menu traduction 11

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     if request.host.split('.').last == "fr"
       link_to "in english", "http://www.netheos.com#{request.env['PATH_INFO']}"
     else
-      link_to "en français", "http://www.netheos.fr#{request.env['PATH_INFO']}"
+      link_to "en francais", "http://www.netheos.fr#{request.env['PATH_INFO']}"
     end
   end
   
