4315acfecb573be89fe891a2f965a00c1bbc4c95
Olivier Detour <o.detour@netheos.net>
2012-12-03 09:36:32 +0100

    menu traduction 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,5 @@
 module ApplicationHelper
   
-  #Return language options
-#  def language
-#    if request.host.split('.').last == "fr"
-#      link_to ("in english", "http://www.netheos.com"+request.env['PATH_INFO'])
-#    else
-#      link_to ("en français", "http://www.netheos.fr"+request.env['PATH_INFO'])
-#    end
-#  end
-  
   # Return good title
   def title
     base_title = "Netheos"
